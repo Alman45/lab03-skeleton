d77faa41257517d399e43a1f8a8a7463b165ad9d
Ali Dali <alman45ad@gmail.com>
2017-09-11 15:33:20 -0400

    Exercise A finished

--- app/src/main/java/course/labs/activitylab/ActivityOne.java
+++ app/src/main/java/course/labs/activitylab/ActivityOne.java
@@ -13,7 +13,19 @@ public class ActivityOne extends Activity {
 		// string for logcat documentation
 		private final static String TAG = "Lab-ActivityOne";
 
-		// lifecycle counts
+
+		TextView tvCreate, tvStart, tvResume, tvPause, tvDestroy, tvStop, tvRestart;
+
+		int onCreateCounter = 0 ;
+		int onStartCounter = 0 ;
+		int onResumeCounter = 0 ;
+		int onPauseCounter = 0 ;
+		int onStopCounter = 0 ;
+		int onDestroyCounter = 0 ;
+		int onRestartCounter = 0 ;
+
+
+	// lifecycle counts
 		//TODO: Create 7 counter variables, each corresponding to a different one of the lifecycle callback methods.
 		//TODO:  increment the variables' values when their corresponding lifecycle methods get called.
 		
@@ -21,10 +33,20 @@ public class ActivityOne extends Activity {
 		protected void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.activity_one);
-			
+
+			onCreateCounter++;
 			//Log cat print out
-			Log.i(TAG, "onCreate called");
-			
+			Log.i(TAG, "onCreate called ");
+
+			tvCreate = (TextView)findViewById(R.id.create);
+			tvStart = (TextView)findViewById(R.id.start);
+			tvResume = (TextView)findViewById(R.id.resume);
+			tvPause = (TextView)findViewById(R.id.pause);
+			tvDestroy = (TextView)findViewById(R.id.destroy);
+			tvStop = (TextView)findViewById(R.id.stop);
+			tvRestart = (TextView)findViewById(R.id.restart);
+
+			tvCreate.setText(Integer.toString(onCreateCounter));
 			//TODO: update the appropriate count variable & update the view
 		}
 
@@ -40,14 +62,62 @@ public class ActivityOne extends Activity {
 		@Override
 		public void onStart(){
 			super.onStart();
-			
+
+			onStartCounter++;
+			tvStart.setText("onStart() calls: " +Integer.toString(onStartCounter));
+
 			//Log cat print out
 			Log.i(TAG, "onStart called");
 			
 			//TODO:  update the appropriate count variable & update the view
 		}
 
-	    // TODO: implement 5 missing lifecycle callback methods
+		@Override
+		public void onResume(){
+			super.onResume();
+			onResumeCounter++;
+			tvResume.setText("onResume() calls: " +Integer.toString(onResumeCounter));
+			Log.i(TAG, "onResume called ");
+		}
+
+		@Override
+		public void onPause(){
+			super.onPause();
+			onPauseCounter++;
+			tvPause.setText("onPause() calls: " +Integer.toString(onPauseCounter));
+
+			Log.i(TAG, "onPause called ");
+		}
+
+		@Override
+		public void onStop(){
+			super.onStop();
+			onStopCounter++;
+			tvStop.setText("onStop() calls: " +Integer.toString(onStopCounter));
+
+			Log.i(TAG, "onStop called " );
+		}
+
+		@Override
+		public void onDestroy(){
+			super.onDestroy();
+			onDestroyCounter++;
+			tvDestroy.setText("onDestroy() calls: " +Integer.toString(onDestroyCounter));
+
+			Log.i(TAG, "onDestroy called " );
+		}
+
+		@Override
+		public void onRestart(){
+			super.onRestart();
+			onRestartCounter++;
+			tvRestart.setText("onRestart() calls: " + Integer.toString(onRestartCounter));
+
+			Log.i(TAG, "onRestart called ");
+		}
+
+
+	// TODO: implement 5 missing lifecycle callback methods
 
 		@Override
 		public void onSaveInstanceState(Bundle savedInstanceState){
